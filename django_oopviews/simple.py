150dc58989ccc58312608be0e7682c8dc41ce11e
melsdoerfer <michael@elsdoerfer.com>
2009-03-16 02:32:17 +0100

    Extended SimpleView to allow sharing a base context between views.

--- django_oopviews/simple.py
+++ django_oopviews/simple.py
@@ -1,4 +1,9 @@
-from base import View
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from base import View, create_view
+
+
+__all__ = ('SimpleView', 'create_view',)
 
 
 class SimpleView(View):
@@ -11,19 +16,41 @@ class SimpleView(View):
             args = ['id']
             kwargs = {'limit': 30}
 
-            def by_author(self):
+            def __call__(self):
                 return something(self.request, self.id, self.limit)
+            def by_author(self):
+                # ...
             def by_publisher(self):
                 # ...
             def by_most_read(self):
                 # ...
 
         book = create_view(BookView)
+        book(request, 10)
         book.by_author(request, 10)
         book.by_publisher(request, 10, limit=100)
 
     Note that ``request`` is automatically shared and does not need
     to be specified in ``args``.
+
+    There is a mechanism to share context values between the different
+    subviews:
+
+        class BookView(SimpleView):
+            args = ['id']
+
+            def _init_context():
+                self.book = get_object_or_404(Book, pk=self.id)
+                return {'book': self.book}
+
+            def __call__():
+                something_with(self.book)
+                ...
+                return 'template.html', {'foo': bar}
+
+    The context returned by ``__call__`` will be merged with the base
+    context returned by ``_init_context``, and used to render the
+    template.
     """
 
     args = []
@@ -45,3 +72,19 @@ class SimpleView(View):
                 setattr(self, name, args.pop(0))
         for name, default in shared_kwargs.items():
             setattr(self, name, kwargs.pop(name, default))
+
+        self._base_context = self._init_context()
+
+    def __after__(self, response):
+        if not isinstance(response, tuple):
+            return response
+        template_name, context = response
+        self._base_context.update(context)
+        return self._render(template_name, self._base_context)
+
+    def _init_context(self):
+        return {}
+
+    def _render(self, template_name, context):
+        return render_to_response(template_name, context,
+            context_instance=RequestContext(self.request))
